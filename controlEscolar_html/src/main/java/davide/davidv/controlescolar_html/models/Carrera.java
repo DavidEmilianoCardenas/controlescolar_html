5eccd442cfdb83232807ff696fd0e25a5026e9e6
David <dvazquez@institutmvm.cat>
2024-03-18 13:05:16 +0100

    Estructura de carpetas

---
+++ controlEscolar_html/src/main/java/davide/davidv/controlescolar_html/models/Carrera.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package davide.davidv.controlescolar_html.models;
+
+/**
+ *
+ * @author Mati
+ */
+public class Carrera {
+    String nombre;
+
+    public Carrera(String nombre) {
+        this.nombre = nombre;
+    }
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
